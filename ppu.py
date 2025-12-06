c9c33799873aaa7fdb39b852a4a4167d1a010078
doomertheboomer <hackerdoomer@gmail.com>
2025-12-06 21:44:56 +0700

    fix reading from 0x2007

--- ppu.py
+++ ppu.py
@@ -20,6 +20,16 @@ class dmppu:
         # skibidi dma
         self.rambus.cpumem[0x4014] = 0 # oamdma
         self.readRegisters()
+        
+        # constants
+        self.ctrlFlags = {
+            'i': 2, # ppudata address incrament
+            's': 3, # sprite pattern table addr
+            'b': 4, # bg pattern table addr
+            'h': 5, # sprite size
+            'p': 6, # ppu master/slave
+            'v': 7 # vblank NMI (INTERRUPT!!!)
+        }
     
     def readRegisters(self):
         self.ctrl = self.rambus.cpumem[0x2000]
@@ -51,7 +61,25 @@ class dmppu:
         
         # write new bytes to high/low ptr
         self.__ppuintlAddr |= (self.addr << highptr)
-                
+    
+    def ctrlFlagGet(self, flag):
+        # return an int for this
+        if flag == 'n':
+            return self.ctrl & 0b00000011
+        if flag[0] in self.__ctrlFlags:
+            return bool((self.ctrl >> self.__ctrlFlags[flag[0]]) & 1)
+        
+    def ctrlFlagSet(self, flag, enable):
+        # TODO: writing to N flag lol
+        if flag == 'n':
+            pass
+        if flag[0] in self.__ctrlFlags:
+            mask = 1 << self.__ctrlFlags[flag[0]]
+            if enable:
+                self.ctrl |= mask
+            else:
+                self.ctrl &= ~mask
+                    
     def fetch(self):
         self.readRegisters()
         
@@ -60,8 +88,10 @@ class dmppu:
         # upload current byte in header first before processing the rest
         if self.rambus.ppu2007Read: # only update data when 2007 is read from the CPU
             self.data = self.rambus.memoryReadPPU(self.__intlAddr)
-            
-        self.__updateAddr() # weird internal state update for addr register
+        
+        # do not update if read
+        if not (self.rambus.ppu2007Read or self.rambus.ppu2007Write):
+            self.__updateAddr() # weird internal state update for addr register
         
         # if accessing palette data, upload immediately instead of on next cycle
         if self.rambus.ppu2007Read:
@@ -71,6 +101,10 @@ class dmppu:
         elif self.rambus.ppu2007Write:
             self.rambus.memoryWritePPU(self.__intlAddr, self.data)
             self.rambus.ppu2007Write = False
+        
+        # incrament after read
+        if self.rambus.ppu2007Read or self.rambus.ppu2007Write:
+            self.__intlAddr += (int(self.ctrlFlagGet('i')) * 31 + 1)
         
         self.writeRegisters()
         
